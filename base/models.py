cd7c77c23d124a92465f34657221d8a5e57b602e
Yash <yashraj504300@gmail.com>
2024-05-12 11:54:47 +0530

    company model added

--- base/models.py
+++ base/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+from re import M
 
+
+class Company(models.Model):
+    name = models.CharField(max_length = 200)
+    bio = models.TextField(max_length = 250, null = True,  blank = True)
+
+    def __str__(self):
+        return self.name
 
 class Advocate(models.Model):
     username = models.CharField(max_length=200)
